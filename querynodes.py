29fe258da786357d266d70a094a61de980230158
backlie <bati26@163.com>
2017-08-21 14:22:30 +0800

    Change to function based

--- querynodes.py
+++ querynodes.py
@@ -1,13 +1,15 @@
 import requests
 import json
 
-url = 'http://192.168.64.135:8181/restconf/operational/opendaylight-inventory:nodes/'
-username = 'admin'
-password = 'admin'
-headers = {
-    'Authorization' : 'Basic YWRtaW46YWRtaW4='
-    }
-result = requests.get(url, headers=headers)
-print result
-outcome = json.loads (result.text)
-print outcome.keys()
+def odl_check_nodes(controller_address, auth_head):
+    url='http://'+controller_address+':8181/restconf/operational/opendaylight-inventory:nodes/'
+    headers={ 'Authorization' : auth_head }
+    return requests.get(url,headers=headers)
+
+if __name__=='__main__':
+    controller_address='10.69.35.85'
+    auth_head='Basic YWRtaW46YWRtaW4='
+    result=odl_check_nodes(controller_address=controller_address,auth_head=auth_head)
+    print result
+    outcome=json.loads(result.text)
+    print outcome
